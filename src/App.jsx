7d32ee385ba632b66caf3bee8dd9e78a7e11c290
yulisa malca <yulisa.malca@gmail.com>
2023-12-06 04:43:08 -0500

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <div className='container'>
+        <h1>Ideas Digitales</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro vel asperiores vero non cum obcaecati eos quia. Sunt voluptatibus, nemo amet vitae consequuntur beatae quasi aliquam soluta in sit architecto.</p>
+      </div>
+    </header>
+    <section>
+      <div className='container'>
+        <h2>nosotros</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus provident quo obcaecati aspernatur voluptates ipsa molestias quod, recusandae blanditiis alias velit cumque officia dolor sequi corporis ut esse, numquam doloribus?</p>
+      </div>
+    </section>
+    <section>
+      <div className='container'>
+        <h2>noticias</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus provident quo obcaecati aspernatur voluptates ipsa molestias quod, recusandae blanditiis alias velit cumque officia dolor sequi corporis ut esse, numquam doloribus?</p>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default App
